9401d46f4f86383700ea1367635cf40016f7f534
santesden <santes.adventures@gmail.com>
2024-06-10 21:09:57 +0200

    add categories to prifile in terms of each individual

--- users/signals.py
+++ users/signals.py
@@ -8,13 +8,13 @@ from .models import Profile, Category
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        profile = Profile.objects.create(user=instance)
         default_categories = ['Entertainment', 'Food', 'Transportation', 'Health', 'Shopping', 'Savings']
-        for name in default_categories:
-            Category.objects.create(name=name, user=instance)
+        for new_category in default_categories:
+            category_obj, created = Category.objects.get_or_create(name=new_category)
+            profile.categories.add(category_obj)
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
-
