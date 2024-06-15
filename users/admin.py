a0e3f5516774368765d99fa49d213e97d082a810
santesden <santes.adventures@gmail.com>
2024-06-15 12:00:32 +0200

    added better admin view

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Budget, Category, BalanceChange
 
 admin.site.register(Profile)
+admin.site.register(Budget)
+admin.site.register(Category)
+admin.site.register(BalanceChange)
