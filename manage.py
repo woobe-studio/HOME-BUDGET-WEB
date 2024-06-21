e85f1eec39af9147ebf99fd159c9b202ac1e168f
santesden <santes.adventures@gmail.com>
2024-06-21 15:04:50 +0200

    added waiting for database

--- manage.py
+++ manage.py
@@ -2,14 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import time
+
 from django.core.management import call_command
 from dotenv import load_dotenv
-from psycopg2 import OperationalError
+from psycopg2 import connect, OperationalError
 
 def create_database():
-    """Create the database if it doesn't exist."""
-    from psycopg2 import connect
-
     load_dotenv(dotenv_path='data.env')
     db_name = os.getenv('DB_NAME')
     db_user = os.getenv('DB_USER')
@@ -18,26 +17,41 @@ def create_database():
     db_port = os.getenv('DB_PORT')
 
     if db_name and db_user and db_password and db_host and db_port:
+        conn = None
         try:
-            conn = connect(dbname='postgres', user=db_user, password=db_password, host=db_host, port=db_port)
-            conn.autocommit = True
-            cur = conn.cursor()
+            attempts = 0
+            max_attempts = 3  # Maximum number of connection attempts
+            delay_seconds = 3  # Delay in seconds between attempts
 
-            cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", (db_name,))
-            exists = cur.fetchone()
+            while attempts < max_attempts:
+                attempts += 1
+                try:
+                    conn = connect(dbname='postgres', user=db_user, password=db_password, host=db_host, port=db_port)
+                    conn.autocommit = True
+                    cur = conn.cursor()
 
-            if not exists:
-                cur.execute(f"CREATE DATABASE {db_name}")
-                print(f"Database '{db_name}' created.")
-            else:
-                print(f"Database '{db_name}' already exists.")
-        except OperationalError as e:
-            print(f"Error creating database '{db_name}': {e}")
+                    cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", (db_name,))
+                    exists = cur.fetchone()
+
+                    if not exists:
+                        cur.execute(f"CREATE DATABASE {db_name}")
+                        print(f"Database '{db_name}' created.")
+                    else:
+                        print(f"Database '{db_name}' already exists.")
+
+                    break  # If connection and database operations succeed, break out of the loop
+                except OperationalError as e:
+                    print(f"Error creating database '{db_name}': {e}")
+                    print(f"Attempt {attempts} of {max_attempts}. Retrying in {delay_seconds} seconds.")
+                    time.sleep(delay_seconds)  # Delay before the next connection attempt
+        except Exception as e:
+            print(f"An error occurred: {e}")
         finally:
             if conn:
                 conn.close()
     else:
         print("Database credentials not provided in environment variables.")
+
 
 def create_superuser():
     """Create superuser if not exists."""
@@ -61,6 +75,7 @@ def run_migrations():
     """Run migrations."""
     call_command('makemigrations')
     call_command('migrate')
+    time.sleep(3)
 
 def main():
     """Run administrative tasks."""
@@ -68,7 +83,8 @@ def main():
     try:
         from django.core.management import execute_from_command_line
         import django
-
+        print("WAITING FOR DATABASE")
+        time.sleep(3)
         # Create the database if it doesn't exist
         create_database()
 
