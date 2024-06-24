46c69cffd935bbeddfbafede08e2b1020469f2c2
santesden <santes.adventures@gmail.com>
2024-06-24 16:44:08 +0200

    working now on docker on www server

--- user_management/settings.py
+++ user_management/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS', 'localhost')).split(' ')
 ALLOWED_HOSTS.append('0.0.0.0')
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UserConfig',
     'social_django',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -55,8 +56,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+ ]
 
-]
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'user_management.urls'
 
